2e3d5aa899afb42397a81fc38bb54d42a3a99124
Brian Jordan <bcjordan@gmail.com>
2014-08-02 22:11:57 -0700

    Add mailer preview stub

--- test/mailers/previews/screenshot_mailer_preview.rb
+++ test/mailers/previews/screenshot_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/screenshot_mailer
 class ScreenshotMailerPreview < ActionMailer::Preview
-
+  def result_email()
+    ScreenshotMailer.result_email(Snapshot.last, Diff.last)
+  end
 end
