f0a2d944f5c38936f30f0b79cc30b656d45dca45
Brian Jordan <bcjordan@gmail.com>
2014-08-02 17:07:58 -0700

    Add debug

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,6 +1,8 @@
 class SnapshotsController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   def trigger
+    puts "Params are: "
+    puts params
     ScreenshotMailer.result_email(params[:user], params[:url]).deliver
   end
 end
